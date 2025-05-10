3f034f35f590918bcc752b2d5ca9a004323c7c44
ijquenta <ivanjosuequentavargas@gmail.com>
2025-05-09 21:29:51 -0400

    [ADD] It's created  home_screen and update UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,7 +6,11 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [_RecipesCard(context), _RecipesCard(context)]),
+      body: Column(
+          children: <Widget> [
+            _RecipesCard(context),
+          ]
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue[200],
         onPressed: () {
@@ -20,22 +24,21 @@ class HomeScreen extends StatelessWidget {
   Future<void> _showBottom(BuildContext context) {
     return showModalBottomSheet(
       context: context,
-      builder:
-          (context) => Container(
-            width: MediaQuery.of(context).size.width,
-            height: 500,
-            color: Colors.white,
-            child: RecipeForm(),
-          ),
+      builder: (context) => Container(
+        width: MediaQuery.of(context).size.width,
+        height: 500,
+        color: Colors.white,
+        child: RecipeForm(),
+      ),
     );
   }
 
   Widget _RecipesCard(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(16),
       child: Container(
         width: MediaQuery.of(context).size.width,
-        height: 125,
+        height: 100,
         child: Card(
           child: Row(
             children: <Widget>[
@@ -43,29 +46,32 @@ class HomeScreen extends StatelessWidget {
                 height: 125,
                 width: 100,
                 child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
                   child: Image.network(
-                    'https://static.platzi.com/media/uploads/flutter_lasana_b894f1aee1.jpg',
+                    'https://upload.wikimedia.org/wikipedia/commons/7/7e/Silpancho_cochalo.jpg',
                     fit: BoxFit.cover,
                   ),
-                  borderRadius: BorderRadius.circular(10),
                 ),
               ),
               SizedBox(width: 26),
               Column(
+                // Ordenar de manera vertical
                 mainAxisAlignment: MainAxisAlignment.center,
+                // Alinear a la izquierda
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Text(
-                    'Lasaña',
-                    style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
+                    'Silpancho Cochabanbino',
+                    style: TextStyle(fontSize: 16, fontFamily: 'Quicksand',
+                        fontWeight: FontWeight.bold),
                   ),
-                  SizedBox(height: 4),
-                  Container(height: 2, width: 75, color: Colors.blue[200]),
+                  //Container(height: 2, width: 330, color: Colors.blue[200]),
+                  SizedBox(height: 8),
                   Text(
-                    'Alison Menez',
-                    style: TextStyle(fontSize: 16, fontFamily: 'Quicksand'),
+                    'By Alison Menez',
+                    style: TextStyle(fontSize: 13, fontFamily: 'Quicksand',
+                        color: Colors.blue[200]),
                   ),
-                  SizedBox(height: 4),
                 ],
               ),
             ],
